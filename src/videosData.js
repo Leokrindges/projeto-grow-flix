32e76e63d3e87d3e7102dfe7dbbedfc828aa74d8
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-02-07 23:58:08 -0300

    add: link e titulos dos videos da categoria gowcast

--- src/videosData.js
+++ src/videosData.js
@@ -2,28 +2,73 @@ const videos = [
     {
         growcast: [
             {
-                titlo: "",
-                link: "",
+                title: "Growcast #00 | Conheça a Growdev",
+                link: "https://www.youtube.com/embed/aJ-HZuLcKeA?si=lGln1M_V53Fx3Max",
+                image: ""
+            },
+            {
+                title: "Growcast #01 | As profissões do futuro",
+                link: "https://www.youtube.com/embed/7OWT3lfHYvE?si=l5pUjlthbIAHzHBn",
+                image: ""
+            },
+            {
+                title: "Growcast #02 | Como seguir a carreira internacional?",
+                link: "https://www.youtube.com/embed/ci1eQGPK89o?si=HpuCOd-KQebdLrMg",
+                image: ""
+            },
+            {
+                title: "Growcast #03 | Superando os desafios da carreira de Dev!",
+                link: "https://www.youtube.com/embed/WM7pGG7OicQ?si=QynInOvhMmWF451q",
+                image: ""
+            },
+            {
+                title: "Growcast #04 | Agile Master: tudo sobre a profissão!",
+                link: "https://www.youtube.com/embed/2c8VOBZ160Y?si=eIQXW_Nbp3xsfWmt",
+                image: ""
+            },
+            {
+                title: "Growcast #5 | Como iniciar na carreira em Desenvolvimento de Softwares?",
+                link: "https://www.youtube.com/embed/mxnEm03cYPc?si=IuDdBXIW-S0NfPb3",
+                image: ""
+            },
+            {
+                title: "Growcast #06 | Mulheres e inclusão na tecnologia",
+                link: "https://www.youtube.com/embed/m7DyR07KrOE?si=URz1TCVkVve_WLQ7",
+                image: ""
+            },
+            {
+                title: "Growcast #07 | Desenvolvendo profissionais na área de TI",
+                link: "https://www.youtube.com/embed/vW8G9KPwRL0?si=NXD4e8pG06m4KXIH",
+                image: ""
+            },
+            {
+                title: "Growcast #8 | A solução que está transformando o mercado de TI no Brasil",
+                link: "https://www.youtube.com/embed/_Ut2KqAqPyk?si=8rmSNBDIaNaVdOaw",
+                image: ""
+            },
+            {
+                title: "Growcast #9 | Acompanhe com quem realmente entende, tudo sobre o mercado de trabalho no mundo da TI.",
+                link: "https://www.youtube.com/embed/dfDgs0Ive2Q?si=lcY_DkX9yM_FwtlC",
                 image: ""
             },
         ],
         webinaFlutter: [
             {
-                titlo: "",
+                title: "",
                 link: "",
                 image: ""
             },
         ],
         jornadaUxUi: [
             {
-                titlo: "",
+                title: "",
                 link: "",
                 image: ""
             },
         ],
         diversos: [
             {
-                titlo: "",
+                title: "",
                 link: "",
                 image: ""
             },
