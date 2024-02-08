45e22d8de22c10531f7a231303662c2f33c9d583
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-02-07 23:40:53 -0300

    add: arquivo js que será a base de dados dos videos

---
+++ videosData.js
@@ -0,0 +1,32 @@
+const videos = [
+    {
+        growcast: [
+            {
+                titlo: "",
+                link: "",
+                image: ""
+            },
+        ],
+        webinaFlutter: [
+            {
+                titlo: "",
+                link: "",
+                image: ""
+            },
+        ],
+        jornadaUxUi: [
+            {
+                titlo: "",
+                link: "",
+                image: ""
+            },
+        ],
+        diversos: [
+            {
+                titlo: "",
+                link: "",
+                image: ""
+            },
+        ]
+    }
+]
