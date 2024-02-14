d965bc9e228afaf69a29a08d6d91cf164402cda6
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-02-13 23:57:06 -0300

    adicionado modal para rodar o video selecionado

--- src/home.js
+++ src/home.js
@@ -1,6 +1,3 @@
-// const cardGrowcast = document.getElementById('body_card').addEventListener('click',() => {
-// alert("teste")
-// })
 procura_videos_growcast()
 procura_videos_webinar_fluter()
 procura_videos_jornada_ux_ui()
@@ -23,12 +20,12 @@ function procura_videos_diversos() {
 }
 
 function criaELementoCard(dados_video, id_elemento) {
-    dados_video.forEach(video => {    
+    dados_video.forEach(video => {
 
-    var colContainer = document.getElementById(`${id_elemento}`);
+        var colContainer = document.getElementById(`${id_elemento}`);
 
         var colDiv = document.createElement('div');
-        colDiv.className = 'col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 mt-4 d-flex justify-content-center';
+        colDiv.className = 'col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-movie mt-4 d-flex justify-content-center';
 
         var cardDiv = document.createElement('div');
         cardDiv.className = 'card border border-0 ';
@@ -37,11 +34,16 @@ function criaELementoCard(dados_video, id_elemento) {
         var img = document.createElement('img');
         img.src = `${video.image}`;
         img.className = 'card-img-top img-fluid';
-        img.alt = '...';
+        img.alt = 'Imagem do video';
 
         var cardBodyDiv = document.createElement('div');
         cardBodyDiv.className = 'card-body bg-black text-white';
         cardBodyDiv.id = 'body_card';
+        cardBodyDiv.setAttribute('data-bs-toggle', 'modal');
+        cardBodyDiv.setAttribute('data-bs-target', '#modal_videos')
+        cardBodyDiv.addEventListener('click', () => {
+           cria_modal(video.link)
+        })
 
         var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
         svg.classList.add = 'bi bi-play-circle me-3';
@@ -57,6 +59,7 @@ function criaELementoCard(dados_video, id_elemento) {
         path2.setAttribute('d', 'M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445');
 
         var span = document.createElement('span');
+        span.className = "ps-3"
         span.textContent = `${video.title}`;
 
         // Adição dos elementos ao documento
@@ -74,5 +77,19 @@ function criaELementoCard(dados_video, id_elemento) {
         colContainer.appendChild(colDiv);
 
     });
+
+}
+
+function cria_modal(link_video) {
+    
+    const modal_videos = document.getElementById('link_video')
+    // class="w-100 h-100"
+    const modal = `
+    <iframe class="w-100 h-100"  src="${link_video}"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>      
+    `
+    modal_videos.innerHTML = modal
 
 }
